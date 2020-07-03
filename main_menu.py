6c64e03cf4ffd32a971700f16bacb63ed78cdb15
Matthew Morgan <matthew.d.morgan@gmail.com>
2020-07-03 14:29:57 -0400

    Added menu options and enabled all three main scripts

--- main_menu.py
+++ main_menu.py
@@ -1,9 +1,12 @@
 import BGG_GameID_Collector
-#import PAX_Title_Corrector
-#import BGG_Metadata_Collector
+import PAX_Title_Corrector
+import BGG_Metadata_Collector
 
-
-import sys
+from pathlib import Path #used for printing current working directory (cwd)
+import sys #used to quit script
+import csv
+import openpyxl
+from difflib import get_close_matches
 
 def print_menu():
     print (30 * '-')
@@ -12,14 +15,15 @@ def print_menu():
     print ("1. BGG ID# Index Update")
     print ("2. Game Title Matching")
     print ("3. Game Metadata Collection")
-    print ("4. Exit")
+    print ("4. Print current working directory")
+    print ("5. Exit")
     print (30 * '-')
 
 loop = True
 
 while loop:  ## Continues executing until loop is false
     print_menu()
-    choice = int(input('Please enter your choice [1-4]: '))
+    choice = int(input('Please enter your choice [1-5]: '))
 
     ### Take actions as per selected menu-option ###
     if choice == 1:
@@ -47,19 +51,21 @@ while loop:  ## Continues executing until loop is false
 
 
     elif choice == 2:
-            print ("Starting matching of game titles...")
-
-
+            print ("Starting matching of game titles. Initialization will take some time...")
+            PAX_Title_Corrector.main()
 
     elif choice == 3:
             print ("Starting collection of metadata...")
-
+            BGG_Metadata_Collector.data_collect()
 
 
     elif choice == 4: 
+            print('Current working directory is: ' + str(Path.cwd()))
+
+
+    elif choice == 5:
             print ("Exiting scipt. Have a nice day!")
             sys.exit()
-
 
 
     else:    ## default ##
